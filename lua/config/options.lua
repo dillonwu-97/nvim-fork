8084416378f4c5cef66874eb0fb89922113c44dd
dillon <dillonwu999@gmail.com>
2025-01-20 10:34:12 -0500

    readme

---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+---- Keymaps
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus" -- Connect neovim clipboard with terminal clipboard
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.guifont = "monospace:h17"
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.linebreak = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = false --
+vim.opt.numberwidth = 4
+vim.opt.shiftwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 5
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.wrap = true
